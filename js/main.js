cda76b5897c9d7883f5a96f8eb187ebe7c35c19a
Oeromilk <oero.mccomb@gmail.com>
2016-09-25 17:53:13 -0400

    Not finished but functioning

--- js/main.js
+++ js/main.js
@@ -5,14 +5,13 @@
   var mintues = document.getElementById('minutes');
   var seconds = document.getElementById('seconds');
   var bar = document.getElementById('inner-bar');
-  var hover = document.getElementById('elapsed-time');
-  //var body = document.getElementById('body');
+  var time = document.getElementById('elapsed-time');
+  var body = document.getElementById('body');
   var currentDate = new Date();
   var currentHours = currentDate.getHours();
   var currentMinutes = currentDate.getMinutes();
   var currentSeconds = currentDate.getSeconds();
-  var backGroundColor =  ["FF0000", "FF6400", "FFF700", "68FF00", "00FFE0", "0055FF"];
-
+  var hexStr = "#" + currentHours + currentMinutes + currentSeconds;
 
   function displayTime (){
     var currentDate = new Date();
@@ -24,24 +23,10 @@
     hours.textContent = ("00" + currentHours).slice(-2);
     minutes.textContent = ("00" + currentMinutes).slice(-2);
     seconds.textContent = ("00" + currentSeconds).slice(-2);
-
-    //console.log(minutePercentage);
+    body.style.backgroundColor = hexStr;
   }
 
+  displayTime();
   window.setInterval(displayTime, 1000);
-  setInterval(change, 1000);
-
-  var i = 0;
-  function change() {
-    var doc = document.getElementById("body");
-    var color = ["black", "blue", "brown", "green"];
-    doc.style.backgroundColor = color[i];
-    i = (i + 1) % color.length;
-  }
-  console.log(change);
-
-  hover.addEventListener("mouseover", function(event) {
-    event.target.style.color = "orange";
-  } );
 
 }())
