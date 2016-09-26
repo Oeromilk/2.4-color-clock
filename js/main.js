2affe16ca505df5b7ee371d2bb7ac4d4521a06f9
Oeromilk <oero.mccomb@gmail.com>
2016-09-26 14:39:35 -0400

    Final working changes

--- js/main.js
+++ js/main.js
@@ -2,16 +2,23 @@
   "use strict";
 
   var hours = document.getElementById('hours');
-  var mintues = document.getElementById('minutes');
+  var minutes = document.getElementById('minutes');
   var seconds = document.getElementById('seconds');
   var bar = document.getElementById('inner-bar');
   var time = document.getElementById('elapsed-time');
   var body = document.getElementById('body');
+
   var currentDate = new Date();
   var currentHours = currentDate.getHours();
   var currentMinutes = currentDate.getMinutes();
   var currentSeconds = currentDate.getSeconds();
-  var hexStr = "#" + currentHours + currentMinutes + currentSeconds;
+
+  var isHovering = false;
+
+  var currentRed = ('0' + currentHours.toString(16)).slice(-2);
+  var currentGreen = ('0' + currentMinutes.toString(16)).slice(-2);
+  var currentBlue = ('0' + currentSeconds.toString(16)).slice(-2);
+  var colorCode = '#' + currentRed + currentGreen + currentBlue;
 
   function displayTime (){
     var currentDate = new Date();
@@ -20,13 +27,36 @@
     var currentSeconds = currentDate.getSeconds();
     var minutePercentage = ((currentSeconds / 59) * 100);
     bar.style.width = minutePercentage + "%";
-    hours.textContent = ("00" + currentHours).slice(-2);
-    minutes.textContent = ("00" + currentMinutes).slice(-2);
-    seconds.textContent = ("00" + currentSeconds).slice(-2);
-    body.style.backgroundColor = hexStr;
+
+    if(isHovering === false){
+      hours.textContent = ('0' + currentHours).slice(-2);
+      minutes.textContent = ('0' + currentMinutes).slice(-2);
+      seconds.textContent = ('0' + currentSeconds).slice(-2);
+    } else {
+      hours.textContent = ('0' + currentHours.toString(16)).slice(-2);
+      minutes.textContent = ('0' + currentMinutes.toString(16)).slice(-2);
+      seconds.textContent = ('0' + currentSeconds.toString(16)).slice(-2);
+    }
+
+    body.style.backgroundColor = colorCode;
+
   }
 
   displayTime();
-  window.setInterval(displayTime, 1000);
+  window.setInterval(displayTime, 100);
+
+  time.addEventListener('mouseover', mouseOverHandler);
+
+  function mouseOverHandler(){
+    isHovering = true;
+  }
+
+
+
+  time.addEventListener('mouseout', mouseOffHandler);
+
+  function mouseOffHandler(){
+    isHovering = false;
+  }
 
 }())
